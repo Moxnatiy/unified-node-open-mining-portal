d5d91b2f7165f8af50591bea008749a8d756e337
Moxnatiy <shnall@ukr.net>
2015-04-04 22:05:33 +0300

    Update profitSwitch.js
    Changed logger.warning to logger.warn (we use the log4js)
    I do not quite understand the need for function   process.on('message', function(message)  but it's work
    
    TO DO 
    fs.writeFile('~/unomp/website/static/' + algo + '.txt', bestBtcPerMhPerHour.toFixed(8));
    dont work correct path if wrong, changed to 
    fs.writeFile( + algo + '.txt', bestBtcPerMhPerHour.toFixed(8),function (err) 
    file writen in ints.js forder but its NaN.txt , but no error please deal with ways

--- libs/profitSwitch.js
+++ libs/profitSwitch.js
@@ -3,7 +3,7 @@ var net    = require('net');
 var bignum = require('bignum');
 var algos  = require('merged-pooler/lib/algoProperties.js');
 var util   = require('merged-pooler/lib/util.js');
-
+var fs     = require('fs');
 var Cryptsy  = require('./apiCryptsy.js');
 var Poloniex = require('./apiPoloniex.js');
 var Mintpal  = require('./apiMintpal.js');
@@ -39,15 +39,26 @@ module.exports = function(logger){
                 }
                 break;
         }
+
+		
+		        Object.keys(poolConfigs).forEach(function(coin){
+
+            var poolConfig = poolConfigs[coin];
+            var algo       = poolConfig.coin.algorithm;
+
+            if (!profitStatus.hasOwnProperty(algo)) {
+                profitStatus[algo] = {};
+            }
         var coinStatus = {
             name: poolConfig.coin.name,
             symbol: poolConfig.coin.symbol,
             difficulty: 0,
             reward: (parseInt(poolConfig.coin.reward) || 0),
             exchangeInfo: {}
-        };
-        profitStatus[algo][poolConfig.coin.symbol] = coinStatus;
-        symbolToAlgorithmMap[poolConfig.coin.symbol] = algo;
+            };
+            profitStatus[algo][poolConfig.coin.symbol] = coinStatus;
+            symbolToAlgorithmMap[poolConfig.coin.symbol] = algo;
+        });
     });
     
     var populateProfitStatus = function() {
@@ -71,6 +82,7 @@ module.exports = function(logger){
             symbolToAlgorithmMap[poolConfig.coin.symbol] = algo;
         });
     }
+
 
     var checkProfitStatus = function() {
 
@@ -589,7 +601,7 @@ module.exports = function(logger){
             // some shitcoins dont provide target, only bits, so we need to deal with both
             var target = response.target ? bignum(response.target, 16) : util.bignumFromBitsHex(response.bits);
             coinStatus.difficulty = parseFloat((diff1 / target.toNumber()).toFixed(9));
-            logger.warning(logSystem, symbol, 'difficulty is ' + coinStatus.difficulty);
+            logger.warn(logSystem, symbol, 'difficulty is ' + coinStatus.difficulty);
 
 	    if (coinStatus.name == 'dogecoindark'){coinStatus.reward = response.coinbasevalue / 1000000;}
 	    else if (coinStatus.name == 'cryptobullion'){coinStatus.reward = response.coinbasevalue / 1000000;}
@@ -639,7 +651,7 @@ module.exports = function(logger){
                             bestCoin = profitStatus[algo][symbol].name;
                         }
                         coinStatus.btcPerMhPerHour = btcPerMhPerHour;
-                        logger.warning(logSystem, 'CALC', 'BTC/' + symbol + ' on ' + exchange + ' with ' + coinStatus.btcPerMhPerHour.toFixed(8) + ' BTC/day per Mh/s');
+                        logger.warn(logSystem, 'CALC', 'BTC/' + symbol + ' on ' + exchange + ' with ' + coinStatus.btcPerMhPerHour.toFixed(8) + ' BTC/day per Mh/s');
                     }
                     if (exchangeData.hasOwnProperty('LTC') && exchangeData['LTC'].hasOwnProperty('weightedBid')){
                         var btcPerMhPerHour = (exchangeData['LTC'].weightedBid * coinStatus.coinsPerMhPerHour) * exchangeData['LTC'].ltcToBtc;
@@ -649,12 +661,14 @@ module.exports = function(logger){
                             bestCoin = profitStatus[algo][symbol].name;
                         }
                         coinStatus.btcPerMhPerHour = btcPerMhPerHour;
-                        logger.warning(logSystem, 'CALC', 'LTC/' + symbol + ' on ' + exchange + ' with ' + coinStatus.btcPerMhPerHour.toFixed(8) + ' BTC/day per Mh/s');
+                        logger.warn(logSystem, 'CALC', 'LTC/' + symbol + ' on ' + exchange + ' with ' + coinStatus.btcPerMhPerHour.toFixed(8) + ' BTC/day per Mh/s');
                     }
                 });
             });
-            logger.warning(logSystem, 'RESULT', 'Best coin for ' + algo + ' is ' + bestCoin + ' on ' + bestExchange + ' with ' + bestBtcPerMhPerHour.toFixed(8) + ' BTC/day per Mh/s');
-			fs.writeFile('~/unomp/website/static/' + algo + '.txt', bestBtcPerMhPerHour.toFixed(8));
+            logger.warn(logSystem, 'RESULT', 'Best coin for ' + algo + ' is ' + bestCoin + ' on ' + bestExchange + ' with ' + bestBtcPerMhPerHour.toFixed(8) + ' BTC/day per Mh/s');
+			fs.writeFile( + algo + '.txt', bestBtcPerMhPerHour.toFixed(8),function (err) {
+  				if (err) throw err;
+});
 
             var client = net.connect(portalConfig.cliPort, function () {
                 client.write(JSON.stringify({
